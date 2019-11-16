707c2db32eee622fb9861dfbc85a1102fff55bbf
Ziyi Huang <ziyihuang@ucsb.edu>
2019-11-16 14:40:10 -0800

    hh - set up a conversion from JSON objects to Java Objects

--- src/main/java/hello/WebController.java
+++ src/main/java/hello/WebController.java
@@ -13,6 +13,8 @@ import java.util.HashMap;
 
 import com.nimbusds.oauth2.sdk.client.ClientReadRequest;
 
+import hello.geojson.FeatureCollection;
+
 @Controller
 public class WebController {
 
@@ -57,16 +59,19 @@ public class WebController {
     }
     
     @GetMapping("/earthquakes/results")
-    public String getEarthquakesResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
-               EqSearch eqSearch) {
-
-           EarthquakeQueryService e = new EarthquakeQueryService();
-
-           model.addAttribute("eqSearch", eqSearch);
+    public String getEarthquakesResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken, EqSearch eqSearch) {
         
-           String json = e.getJSON(eqSearch.getDistance(), eqSearch.getMinmag());
-           model.addAttribute("json", json);
-           return "earthquakes/results";
+        EarthquakeQueryService e = new EarthquakeQueryService();
+        
+        model.addAttribute("eqSearch", eqSearch);
+    
+        String json = e.getJSON(eqSearch.getDistance(), eqSearch.getMinmag());
+        model.addAttribute("json", json);
+        
+        FeatureCollection featureCollection = FeatureCollection.fromJSON(json);
+        model.addAttribute("featureCollection",featureCollection);
+        
+        return "earthquakes/results";
     }
     
 }
