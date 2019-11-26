dab1fd30cbb8c9909b1744b79c4594d132ce2976
Ziyi Huang <ziyihuang@ucsb.edu>
2019-11-25 23:54:41 -0800

    add real location query service

--- src/main/java/earthquakes/services/LocationQueryService.java
+++ src/main/java/earthquakes/services/LocationQueryService.java
@@ -18,47 +18,36 @@ import org.springframework.web.client.HttpClientErrorException;
 
 public class LocationQueryService {
 
-    private Logger logger = LoggerFactory.getLogger(EarthquakeQueryService.class);
+    private Logger logger = LoggerFactory.getLogger(LocationQueryService.class);
 
     
     public String getJSON(String location) {
-        
-        String fakeJson = "{ \"key\": \"value\" }";
-        String json = fakeJson;
-        logger.info("json=" + json);
-        return json;
      
-        /*
         RestTemplate restTemplate = new RestTemplate();
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-            headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
-            HttpEntity<String> entity = new HttpEntity<>("body", headers);
+        HttpEntity<String> entity = new HttpEntity<>("body", headers);
 
-            String uri = "https://earthquake.usgs.gov/fdsnws/event/1/query";
-            double ucsbLat = 34.4140;
-            double ucsbLong = -119.8489;
-            String params = String.format("?format=geojson&minmagnitude=%d&maxradiuskm=%d&latitude=%f&longitude=%f",
-               minmag,distance,ucsbLat,ucsbLong);
+        String uri = "https://nominatim.org/release-docs/develop/api/Search/";
+        String params = String.format("%s?format=json", location);
 
-            String url = uri + params;
-            logger.info("url=" + url);
+        String url = uri + params;
+        logger.info("url=" + url);
 
-            String retVal="";
-            try {
-                ResponseEntity<String> re = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-                 MediaType contentType = re.getHeaders().getContentType();
-                HttpStatus statusCode = re.getStatusCode();
-                retVal = re.getBody();
+        String retVal="";
+        try {
+            ResponseEntity<String> re = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+            MediaType contentType = re.getHeaders().getContentType();
+            HttpStatus statusCode = re.getStatusCode();
+            retVal = re.getBody();
             } catch (HttpClientErrorException e) {
                 retVal = "{\"error\": \"401: Unauthorized\"}";
             }
-            logger.info("from EarthquakeQueryService.getJSON: " + retVal);
+            logger.info("from LocationQueryService.getJSON: " + retVal);
             return retVal;
-         
-         **/
     }
-     
+    
 }
